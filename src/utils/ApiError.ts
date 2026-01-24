d535c9529998cbf03181b1b82f6847259de085d0
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2026-01-24 18:28:59 -0500

    feat: clean up api error code

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,4 +1,4 @@
-import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from "http-status-codes";
 
 interface ErrorDefinition {
   code: string;
@@ -68,79 +68,54 @@ class ApiError extends BaseError {
 /**
  * Check if error is an API specific error
  */
-export const IsApiError = (err: any): boolean =>
-  err instanceof ApiError ? err.isOperational : false;
+export const IsApiError = (err: unknown): boolean => (err instanceof ApiError ? err.isOperational : false);
 
 export { ApiError };
 
 export class NotFoundError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.NOT_FOUND.message,
-    type: string = DEFAULT_ERRORS.NOT_FOUND.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.NOT_FOUND.message, type: string = DEFAULT_ERRORS.NOT_FOUND.code) {
     super(message, StatusCodes.NOT_FOUND, type);
   }
 }
 
 export class BadRequestError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.BAD_REQUEST.message,
-    type: string = DEFAULT_ERRORS.BAD_REQUEST.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.BAD_REQUEST.message, type: string = DEFAULT_ERRORS.BAD_REQUEST.code) {
     super(message, StatusCodes.BAD_REQUEST, type);
   }
 }
 
 export class ValidationError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.VALIDATION.message,
-    type: string = DEFAULT_ERRORS.VALIDATION.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.VALIDATION.message, type: string = DEFAULT_ERRORS.VALIDATION.code) {
     super(message, StatusCodes.BAD_REQUEST, type);
   }
 }
 
 export class UnauthorizedError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.UNAUTHORIZED.message,
-    type: string = DEFAULT_ERRORS.UNAUTHORIZED.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.UNAUTHORIZED.message, type: string = DEFAULT_ERRORS.UNAUTHORIZED.code) {
     super(message, StatusCodes.UNAUTHORIZED, type);
   }
 }
 
 export class BadTokenError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.BAD_TOKEN.message,
-    type: string = DEFAULT_ERRORS.BAD_TOKEN.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.BAD_TOKEN.message, type: string = DEFAULT_ERRORS.BAD_TOKEN.code) {
     super(message, StatusCodes.UNAUTHORIZED, type);
   }
 }
 
 export class TokenExpiredError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.TOKEN_EXPIRED.message,
-    type: string = DEFAULT_ERRORS.TOKEN_EXPIRED.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.TOKEN_EXPIRED.message, type: string = DEFAULT_ERRORS.TOKEN_EXPIRED.code) {
     super(message, StatusCodes.UNAUTHORIZED, type);
   }
 }
 
 export class ForbiddenError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.FORBIDDEN.message,
-    type: string = DEFAULT_ERRORS.FORBIDDEN.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.FORBIDDEN.message, type: string = DEFAULT_ERRORS.FORBIDDEN.code) {
     super(message, StatusCodes.FORBIDDEN, type);
   }
 }
 
 export class ServerError extends ApiError {
-  constructor(
-    message: string = DEFAULT_ERRORS.SERVER_ERROR.message,
-    type: string = DEFAULT_ERRORS.SERVER_ERROR.code
-  ) {
+  constructor(message: string = DEFAULT_ERRORS.SERVER_ERROR.message, type: string = DEFAULT_ERRORS.SERVER_ERROR.code) {
     super(message, StatusCodes.INTERNAL_SERVER_ERROR, type);
   }
 }
