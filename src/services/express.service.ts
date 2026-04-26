519108781e36af92b0205914e705885c7774dffc
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2026-04-26 19:15:41 -0400

    feat: replace autoload for routes in express service

--- src/services/express.service.ts
+++ src/services/express.service.ts
@@ -1,65 +1,67 @@
-import express, { Express, Router } from "express";
+import express, { Router } from "express";
 import rateLimit from "express-rate-limit";
-import fs from "fs";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
-import { fileURLToPath, pathToFileURL } from "url";
-import { dirname } from "path";
 
 import logger from "./logger.service";
 
 import globalErrorHandler from "../middlewares/errorHandler.middleware.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const routesDir = process.env.NODE_ENV === "production" ? __dirname + "/../../dist/routes/" : __dirname + "/../routes/";
-const routeFiles = fs.readdirSync(routesDir);
-
-let server: Express;
-const routes: Router[] = [];
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-});
-
-const corsOptions = {
-  origin: "*", // TODO replace by valid origin for restrict access (security)
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  credentials: true,
-  optionsSuccessStatus: 204,
-  allowedHeaders: "Content-Type, Authorization, Content-Length",
-};
+import { loginRoutes } from "@/routes/login.routes";
+import { addressRoutes } from "@/routes/address.routes";
+import { calendarEventRoutes } from "@/routes/calendarEvents.routes";
+import { invoiceRoutes } from "@/routes/invoices.routes";
+import { measureRoutes } from "@/routes/measure.routes";
+import { noteRoutes } from "@routes/note.routes";
+import { petRecordRoutes } from "@/routes/petRecord.routes";
+import { userRoutes } from "@/routes/user.routes";
+import { vaccineRoutes } from "@/routes/vaccines.routes";
+import { wormableRoutes } from "@/routes/wormables.routes";
 
 interface ExpressService {
-  init: () => Promise<void>;
+  init: () => void;
 }
 
 const expressService: ExpressService = {
-  init: async (): Promise<void> => {
+  init: (): void => {
     try {
-      // Loading routes automatically
-      for (const file of routeFiles) {
-        // Absolute path to convert to ESIM-compatible URL
-        const routePath = pathToFileURL(routesDir + file).href;
-        const route = await import(routePath);
-        const routeName = Object.keys(route)[0];
-        routes.push(route[routeName]);
-      }
+      const rateLimiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+      });
 
-      server = express();
+      const corsOptions = {
+        origin: "*", // TODO replace by valid origin for restrict access (security)
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        credentials: true,
+        optionsSuccessStatus: 204,
+        allowedHeaders: "Content-Type, Authorization, Content-Length",
+      };
+
+      const routes: Router[] = [
+        loginRoutes,
+        addressRoutes,
+        calendarEventRoutes,
+        invoiceRoutes,
+        measureRoutes,
+        noteRoutes,
+        petRecordRoutes,
+        userRoutes,
+        vaccineRoutes,
+        wormableRoutes,
+      ];
+
+      const server = express();
       server.use(bodyParser.json());
       server.use((cors as (options: cors.CorsOptions) => express.RequestHandler)(corsOptions));
       server.options("*", (cors as (options: cors.CorsOptions) => express.RequestHandler)(corsOptions));
+      server.use(rateLimiter);
+      server.use(helmet());
 
       routes.forEach((route) => {
-        server.use(route);
+        server.use("/api", route);
       });
-
-      server.use(limiter);
-      server.use(helmet());
 
       server.get("/", (_req, res) => {
         res.send("Hello World!");
